4bae774e529b823d0794f13c1992c76cf2c3efa3
yirou89757 <xueyi2017@gmail.com>
2019-11-25 14:27:35 -0500

    Modification

--- Hello.java
+++ Hello.java
@@ -1,6 +1,17 @@
-/* Print hello world! */
+import java.io.*;
+
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String s;
+        String[] cmd = {"ls", "-l"};  // curl localhost:// 
+        try {
+            Process process = Runtime.getRuntime().exec(cmd);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            while ((s = reader.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (Exception e) {
+            // do nothing
+        }
     }
 }
